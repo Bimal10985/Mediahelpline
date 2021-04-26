c55218cb0c0279d2f82b9c901bc98568e2dcfa03
Bimal10985 <bimal10985@gmail.com>
2021-04-26 13:10:06 +0545

    first commit

--- app.js
+++ app.js
@@ -7,5 +7,20 @@ $(function () {
       );
       return false;
     });
+    
+    var swiper = new Swiper('.swiper-container', {
+      slidesPerView: 3,
+      spaceBetween: 30,
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+      //   navigation: {
+      //     nextEl: '.swiper-button-next',
+      //     prevEl: '.swiper-button-prev',
+        
+      // },
+    });
     AOS.refresh();
   });
+ 
